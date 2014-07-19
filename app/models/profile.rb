3ef399d0a1649fd5da404ea84887a6cf5bdba93f
Prelang Builder <builder@prelang.com>
2014-07-19 05:27:48 +0000

    Defined validation for attribute 'facebook' on model 'Profile'

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,5 @@ class Profile < ActiveRecord::Base
   belongs_to :user
   validates_formatting_of :email, using: :email
   validates_formatting_of :url, using: :url
+  validates_formatting_of :facebook, using: :url
 end
