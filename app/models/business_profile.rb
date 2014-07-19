9e4f540e23c9b3039a371a7bd0bba4be200dd7bf
Prelang Builder <builder@prelang.com>
2014-07-19 05:27:46 +0000

    Defined validation for attribute 'linkedin' on model 'BusinessProfile'

--- app/models/business_profile.rb
+++ app/models/business_profile.rb
@@ -3,4 +3,5 @@ class BusinessProfile < ActiveRecord::Base
   has_many :business_profile_categories
   has_many :categories
   validates_formatting_of :facebook, using: :url
+  validates_formatting_of :linkedin, using: :url
 end
