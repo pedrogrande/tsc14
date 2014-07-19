4e1cb2e0e95692436a719990b1402c7393379fc8
Prelang Builder <builder@prelang.com>
2014-07-19 05:27:46 +0000

    Defined validation for attribute 'google' on model 'BusinessProfile'

--- app/models/business_profile.rb
+++ app/models/business_profile.rb
@@ -5,4 +5,5 @@ class BusinessProfile < ActiveRecord::Base
   validates_formatting_of :facebook, using: :url
   validates_formatting_of :linkedin, using: :url
   validates_formatting_of :url, using: :url
+  validates_formatting_of :google, using: :url
 end
