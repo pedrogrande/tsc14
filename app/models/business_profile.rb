7fad2d9c71cd700729689efb5a2cd6502754e535
Prelang Builder <builder@prelang.com>
2014-07-19 05:27:45 +0000

    Defined validation for attribute 'facebook' on model 'BusinessProfile'

--- app/models/business_profile.rb
+++ app/models/business_profile.rb
@@ -2,4 +2,5 @@ class BusinessProfile < ActiveRecord::Base
   belongs_to :category
   has_many :business_profile_categories
   has_many :categories
+  validates_formatting_of :facebook, using: :url
 end
